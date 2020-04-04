fd1918ff719a175a083443ac6f3725823fff44a3
UNKNOWN-AFK <samaymehta2104@gmail.com>
2020-04-04 12:17:24 +0000

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -2,7 +2,7 @@
 clear
 echo -e "\e[4;31m SKB Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-toilet -f mono12 -F border 
+toilet -f mono12 -F border TBomb
 echo "Press Enter To Continue"
 read a1
 if [[ -s update.speedx ]];then
